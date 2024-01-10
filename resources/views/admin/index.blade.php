0ef6c8bc83055a4e0d76b9371960eb72116d6c4f
immanuel segismundo <immansegismundo@gmail.com>
2024-01-11 01:40:12 +0800

    UI chart

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -23,7 +23,34 @@
     .col-md-3 {
         margin-bottom: 15px;
     }
+
+    /* Add this style to make the pie chart smaller */
+    canvas {
+        max-width: 100%;
+        height: auto;
+    }
+
+    /* Add this style to create a row layout */
+    .chart-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+
+    /* Adjust the width of the columns based on your preference */
+    .chart-container .chart-column {
+        width: 100%; /* Requests and Ages span full width */
+        margin-bottom: 15px;
+    }
+
+    /* Adjust the width of the columns based on your preference */
+    .chart-container .half-width-column {
+        width: calc(40% - 10px); /* Blotters and Sex share the row */
+        margin-bottom: 15px;
+    }
 </style>
+
+
 <div class="container">
     <div class="row justify-content-between align-items-center">
         <div class="col-lg-6">
@@ -102,41 +129,31 @@
                     <h1>{{ $settledBlotters }}</h1>
                 </div>
             </a>
-        </div>
-        <div class="col-md-3">
-            <a href="{{ route('blotter.index') }}">
-                <div class="d-card">
-                    <h6>Active blotters:</h6>
-                    <h1>{{ $countUnder18 }}</h1>
-                </div>
-            </a>
-        </div>
-        <div class="col-md-3">
-            <a href="{{ route('blotter.index') }}">
-                <div class="d-card">
-                    <h6>Settled blotters:</h6>
-                    <h1>{{ $count31to50 }}</h1>
-                </div>
-            </a>
-        </div>
-                             
+        </div>                     
     </div>
-    <div><br>
-        <h4>Requests</h4>
-        <canvas id="myBarChart" width="100" height="30"></canvas>
-    </div><br>
-    <div>
-        <h4>Blotters</h4>
-        <canvas id="blotterPieChart" width="300" height="300"></canvas>
 
-    </div>
-    <div>
-        <h4>Sex</h4>
-        <canvas id="myPieChart" width="400" height="400"></canvas>
-    </div>
-    <div>
-        <h4>Ages</h4>
-        <canvas id="myChart" width="200" height="50"></canvas>
+    <br>
+
+    <div class="chart-container">
+        <div class="chart-column">
+            <h4>Requests</h4>
+            <canvas id="myBarChart" width="100" height="30"></canvas>
+        </div>
+
+        <div class="half-width-column">
+            <h4>Blotters</h4>
+            <canvas id="blotterPieChart" width="300" height="300"></canvas>
+        </div>
+
+        <div class="half-width-column">
+            <h4>Sex</h4>
+            <canvas id="myPieChart" width="400" height="400"></canvas>
+        </div>
+
+        <div class="chart-column">
+            <h4>Ages</h4>
+            <canvas id="myChart" width="200" height="50"></canvas>
+        </div>
     </div>
 
 </div>
@@ -239,27 +256,27 @@
 
     //AGE
 
-       var ctx = document.getElementById('myChart').getContext('2d');
-       var myChart = new Chart(ctx, {
-           type: 'bar',
-           data: {
-               labels: ['17 and Below ', '18 to 30', '31 to 50', 'Above 50'],
-               datasets: [{
-                   label: 'Count',
-                   data: [{{ $countUnder18 }}, {{ $count18to30 }}, {{ $count31to50 }}, {{ $countAbove50 }}],
-                   backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                   borderColor: 'rgba(75, 192, 192, 1)',
-                   borderWidth: 1
-               }]
-           },
-           options: {
-               scales: {
-                   y: {
-                       beginAtZero: true
-                   }
-               }
-           }
-       });
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['17 and Below', '18 to 30', '31 to 50', 'Above 50'],
+            datasets: [{
+                label: 'Count',
+                data: [{{ $countUnder18 }}, {{ $count18to30 }}, {{ $count31to50 }}, {{ $countAbove50 }}],
+                backgroundColor: ['rgba(0, 255, 0, 0.2)', 'rgba(0, 0, 255, 0.2)', 'rgba(255, 255, 0, 0.2)', 'rgba(255, 0, 0, 0.2)'],
+                borderColor: ['rgba(0, 255, 0, 1)', 'rgba(0, 0, 255, 1)', 'rgba(255, 255, 0, 1)', 'rgba(255, 0, 0, 1)'],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
    </script>
   
 
